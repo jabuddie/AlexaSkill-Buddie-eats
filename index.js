442c0be4e38dcbe4eb4fa425fa7d9889501c6166
jbuddie <Justine.Buddie@cisecurity.org>
2021-09-14 16:33:21 -0400

    add files and content needed for buddie-eats skill

---
+++ index.js
@@ -0,0 +1,228 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk');
+const foods = require('./foods');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+/* INTENT HANDLERS */
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+        const item = requestAttributes.t(getRandomItem(Object.keys(foods.FOOD_EN_US)));
+
+        const speakOutput = requestAttributes.t('WELCOME_MESSAGE', requestAttributes.t('SKILL_NAME'), item);
+        const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
+
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(repromptOutput)
+            .getResponse();
+    },
+};
+
+const FoodHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'FoodIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+        const itemSlot = handlerInput.requestEnvelope.request.intent.slots.Item;
+        let itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        const cardTitle = requestAttributes.t('DISPLAY_CARD_TITLE', requestAttributes.t('SKILL_NAME'), itemName);
+        const myFoods = requestAttributes.t('FOODS');
+        const food = myFoods[itemName];
+        let speakOutput = "";
+
+        if (food) {
+            sessionAttributes.speakOutput = food;
+            handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+            return handlerInput.responseBuilder
+                .speak(sessionAttributes.speakOutput) // .reprompt(sessionAttributes.repromptSpeech)
+                .withSimpleCard(cardTitle, food)
+                .getResponse();
+        }
+        else{
+            speakOutput = requestAttributes.t('FOOD_NOT_FOUND_MESSAGE');
+            const repromptSpeech = requestAttributes.t('FOOD_NOT_FOUND_REPROMPT');
+            if (itemName) {
+                speakOutput += requestAttributes.t('FOOD_NOT_FOUND_WITH_ITEM_NAME', itemName);
+            } else {
+                speakOutput += requestAttributes.t('FOOD_NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speakOutput += repromptSpeech;
+
+            sessionAttributes.speakOutput = speakOutput; //saving speakOutput to attributes, so we can use it to repeat
+            sessionAttributes.repromptSpeech = repromptSpeech;
+
+            handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+            return handlerInput.responseBuilder
+                .speak(sessionAttributes.speakOutput)
+                .reprompt(sessionAttributes.repromptSpeech)
+                .getResponse();
+        }
+    }
+};
+
+const HelpHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+        const item = requestAttributes.t(getRandomItem(Object.keys(foods.FOOD_EN_US)));
+
+        sessionAttributes.speakOutput = requestAttributes.t('HELP_MESSAGE', item);
+        sessionAttributes.repromptSpeech = requestAttributes.t('HELP_REPROMPT', item);
+
+        return handlerInput.responseBuilder
+            .speak(sessionAttributes.speakOutput)
+            .reprompt(sessionAttributes.repromptSpeech)
+            .getResponse();
+    },
+};
+
+const RepeatHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent';
+    },
+    handle(handlerInput) {
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+        return handlerInput.responseBuilder
+            .speak(sessionAttributes.speakOutput)
+            .reprompt(sessionAttributes.repromptSpeech)
+            .getResponse();
+    },
+};
+
+const ExitHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent');
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speakOutput = requestAttributes.t('STOP_MESSAGE', requestAttributes.t('SKILL_NAME'));
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    },
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        console.log("Inside SessionEndedRequestHandler");
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I don\'t understand the command. Please try again.')
+            .reprompt('Sorry, I don\'t understand the command. Please try again.')
+            .getResponse();
+    },
+};
+
+/* CONSTANTS */
+const skillBuilder = Alexa.SkillBuilders.standard();
+const languageStrings = {
+    en: {
+        translation: {
+            FOODS: foods.FOOD_EN_US,
+            SKILL_NAME: 'Buddie Eats',
+            WELCOME_MESSAGE: 'Welcome to %s, where you can ask about foods for your canine buddie. You can say something like, can buddie eat carrots? Always speak to your vet before modifying your pet\'s diet... Now, what can I help you with?',
+            WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+            DISPLAY_CARD_TITLE: '%s  - Information for %s.',
+            HELP_MESSAGE: 'Need help? You can say things like, get information for shrimp, tell me about popcorn, or can buddie eat bread. Or say stop... Now, what can I help you with?',
+            HELP_REPROMPT: 'You can say things like, get information for shrimp, tell me about popcorn, or can buddie eat bread. Or say stop... Now, what can I help you with?',
+            STOP_MESSAGE: 'Goodbye!',
+            FOOD_REPEAT_MESSAGE: 'Try saying repeat.',
+            FOOD_NOT_FOUND_MESSAGE: 'I\'m sorry, I currently do not know ',
+            FOOD_NOT_FOUND_WITH_ITEM_NAME: 'the information for %s. ',
+            FOOD_NOT_FOUND_WITHOUT_ITEM_NAME: 'that information. ',
+            FOOD_NOT_FOUND_REPROMPT: 'Always call your vet if you\'re unsure. What else can I help with?'
+        },
+    },
+    'en-US': {
+        translation: {
+            FOODS: foods.FOOD_EN_US,
+            SKILL_NAME: 'Buddie Eats'
+        },
+    },
+
+};
+
+// Finding the locale of the user
+const LocalizationInterceptor = {
+    process(handlerInput) {
+        const localizationClient = i18n.use(sprintf).init({
+            lng: handlerInput.requestEnvelope.request.locale,
+            overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+            resources: languageStrings,
+            returnObjects: true
+        });
+
+        const attributes = handlerInput.attributesManager.getRequestAttributes();
+        attributes.t = function (...args) {
+            return localizationClient.t(...args);
+        };
+    },
+};
+
+// getRandomItem
+function getRandomItem(arrayOfItems) {
+    // the argument is an array [] of words or phrases
+    let i = 0;
+    i = Math.floor(Math.random() * arrayOfItems.length);
+    return (arrayOfItems[i]);
+};
+
+/* LAMBDA SETUP */
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        FoodHandler,
+        HelpHandler,
+        RepeatHandler,
+        ExitHandler,
+        SessionEndedRequestHandler
+    )
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
